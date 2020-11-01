0614f8dbaf5f38e58424bf60eb3c08693ba86ccf
cbalieiro <bmoliveira.camila@gmail.com>
2020-10-31 22:07:44 -0300

    Estou com erros no teste, deletei tudo no github e tentei fazer o fork e tudo novamente, mas o erro persiste

--- cipher.js
+++ cipher.js
@@ -1,5 +1,19 @@
 const cipher = {
-  // ...
+  // Para não realizar confusões nas obrigatoriedades, mantive a estrutura de nomeclatura do README cipher.encode(offset, string)
+
+  encode(offset, string) {
+    let textoC = "";
+    for(let i = 0; i < string.length; i++) 
+    {textoC += String.fromCharCode(string.charCodeAt(i)+ offset);}
+    return textoC
+  } , // sem a vírgula não funciona. Isso pq estamos criando dois metodos distintos dentro do objeto.
+
+  decode(offset, string){
+    let textoD = "";
+    for(let i = 0; i < string.length; i++) 
+    {textoD += String.fromCharCode(Math.abs(string.charCodeAt(i)-offset));}
+    return textoD
+  }
 };
 
 export default cipher;
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
+//console.log(cipher);
 
-console.log(cipher);
+const btnCriptografar = window.document.getElementById('btnCodif').addEventListener("click", function chamaEncode() {
+
+    let tCod = window.document.getElementById('txtSringC').value.toUpperCase();
+    let offCod = Number(window.document.getElementById('resOffset').value);
+    
+    const codificado = cipher.encode(offCod,tCod);
+ 
+    let res = window.document.getElementById('res');
+    res.innerHTML = codificado
+
+
+    //console.log(typeof offCod)
+    //console.log(tCod.charCodeAt(0))
+  }
+  ); 
+
+  const btnDescriptografar = window.document.getElementById('btnDescodif').addEventListener("click", function chamaDecode() {
+
+    let tCod = window.document.getElementById('txtSringC').value.toUpperCase();
+    let offCod = Number(window.document.getElementById('resOffset').value);
+    
+    const codificado = cipher.decode(offCod,tCod);
+ 
+    let res = window.document.getElementById('res');
+    res.innerHTML = codificado
+
+
+    //console.log(typeof offCod)
+    //console.log(tCod.charCodeAt(0))
+  }
+  ); 
