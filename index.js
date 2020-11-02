9b3e6f912f61b16be669b669d1020d130266c7b2
cbalieiro <bmoliveira.camila@gmail.com>
2020-11-02 19:13:26 -0300

    ajuste no type error, ajuste no encode\decode delimitando o tamanho do alfabeto, ajustes em nomeclaturas, realizado mudanças no html e css

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 import cipher from './cipher.js';
 //console.log(cipher);
 
-const btnCriptografar = window.document.getElementById('btnCodif').addEventListener("click", function chamaEncode() {
+const btnCriptografar = window.document.getElementById('btnEncode');
+const btnDescriptografar = window.document.getElementById('btnDecode');
 
-    let tCod = window.document.getElementById('txtSringC').value.toUpperCase();
-    let offCod = Number(window.document.getElementById('resOffset').value);
-    
-    const codificado = cipher.encode(offCod,tCod);
- 
-    let res = window.document.getElementById('res');
-    res.innerHTML = codificado
+let res = window.document.getElementById('txtRes');
 
+btnCriptografar.addEventListener("click", function chamaEncode() {
+  let tCod = window.document.getElementById('txtSringC').value.toUpperCase();
+  let offCod = Number(window.document.getElementById('resOffset').value);
+  const codificado = cipher.encode(offCod, tCod);
+  res.innerHTML = codificado;
+}
+);
 
-    //console.log(typeof offCod)
-    //console.log(tCod.charCodeAt(0))
-  }
-  ); 
-
-  const btnDescriptografar = window.document.getElementById('btnDescodif').addEventListener("click", function chamaDecode() {
-
-    let tCod = window.document.getElementById('txtSringC').value.toUpperCase();
-    let offCod = Number(window.document.getElementById('resOffset').value);
-    
-    const codificado = cipher.decode(offCod,tCod);
- 
-    let res = window.document.getElementById('res');
-    res.innerHTML = codificado
-
-
-    //console.log(typeof offCod)
-    //console.log(tCod.charCodeAt(0))
-  }
-  ); 
+btnDescriptografar.addEventListener("click", function chamaDecode() {
+  let tCod = window.document.getElementById('txtSringC').value.toUpperCase();
+  let offCod = Number(window.document.getElementById('resOffset').value);
+  const codificado = cipher.decode(offCod, tCod);
+  res.innerHTML = codificado;
+}
+); 
