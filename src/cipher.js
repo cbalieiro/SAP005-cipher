1c957329ae7edd22eff206a3e11ee12a5692d998
cbalieiro <bmoliveira.camila@gmail.com>
2020-11-02 19:13:26 -0300

    ajuste no type error, ajuste no encode\decode delimitando o tamanho do alfabeto, ajustes em nomeclaturas, realizado mudanças no html e css

--- src/cipher.js
+++ src/cipher.js
@@ -3,15 +3,23 @@ const cipher = {
 
   encode(offset, string) {
     let textoC = "";
-    for(let i = 0; i < string.length; i++) 
-    {textoC += String.fromCharCode(string.charCodeAt(i)+ offset);}
+    if(typeof offset == 'number' && typeof string == 'string'){
+    for(let i = 0; i < string.length; i++)  
+    {textoC += String.fromCharCode(Math.abs((string.charCodeAt(i)+65 + offset)%26)+65);}
+  } else {
+    throw new TypeError();
+  }
     return textoC
   } , // sem a vírgula não funciona. Isso pq estamos criando dois metodos distintos dentro do objeto.
 
   decode(offset, string){
     let textoD = "";
+    if(typeof offset == 'number' && typeof string == 'string'){
     for(let i = 0; i < string.length; i++) 
-    {textoD += String.fromCharCode(Math.abs(string.charCodeAt(i)-offset));}
+    {textoD += String.fromCharCode(Math.abs((string.charCodeAt(i)+65 - offset)%26)+65);}
+  } else {
+    throw new TypeError();
+  }
     return textoD
   }
 };
