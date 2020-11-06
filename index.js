d9141abe40e35c3b537b7fb12fee5351efb0b584
cbalieiro <bmoliveira.camila@gmail.com>
2020-11-06 20:35:20 -0300

    fiz algumas alterações no codigo, mas voltei, pois não me senti satisfeita

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 import cipher from './cipher.js'
 let resultadoDisplay = window.document.getElementById('txtRes');
-
-function chamaEncode() {
+window.document.getElementById('btnEncode').addEventListener("click", function chamaEncode() {
   const textoInput = window.document.getElementById('txtSringE').value.toUpperCase();
   const offsetInput = Number(window.document.getElementById('offsetEncode').value);
   const codificado = cipher.encode(offsetInput, textoInput);
   resultadoDisplay.innerHTML = codificado;
   return codificado
-};
-
-function chamaDecode() {
+})
+window.document.getElementById('btnDecode').addEventListener("click",function chamaDecode() {
   const textoInput = window.document.getElementById('txtSringD').value.toUpperCase();
   const offsetInput = Number(window.document.getElementById('offsetDecode').value);
   const decodificado = cipher.decode(offsetInput, textoInput);
   resultadoDisplay.innerHTML = decodificado;
   return decodificado
-};
-
-window.document.getElementById('btnEncode').addEventListener("click", chamaEncode);
-window.document.getElementById('btnDecode').addEventListener("click", chamaDecode);
-
-
+})
